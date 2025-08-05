d68215ed3c780246893322f1aa1ef5f9ceabe083
Auvryy <sarneandy6@gmail.com>
2025-08-05 18:49:06 +0800

    added scroll animation

--- src/main.js
+++ src/main.js
@@ -135,3 +135,19 @@ projectsBtn.addEventListener('click', () => {
 contactsBtn.addEventListener('click', () => {
   contactsSection.scrollIntoView({behavior: 'smooth'})
 })
+
+//animation scrolling
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('scroll-show')
+    } 
+    // else {
+    //   entry.target.classList.remove('scroll-show')
+    // }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.scroll-hidden')
+hiddenElements.forEach((el) =>  observer.observe(el))
